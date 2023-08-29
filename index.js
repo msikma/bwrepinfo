183973234c8afa65e3efef5a30bc9602f50e6cf7
Michiel Sikma <michiel@sikma.org>
2023-08-30 01:34:58 +0200

    Allow the caller to pass in options

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ import {readLocalBuffer} from './util/local.js'
 /**
  * Loads a replay file into an ArrayBuffer and returns its parsed info.
  */
-export async function getFileRepInfo(filepath) {
+export async function getFileRepInfo(filepath, options = {}) {
   const arrayBuffer = await readLocalBuffer(filepath)
-  return getBufferRepInfo(arrayBuffer)
+  return getBufferRepInfo(arrayBuffer, options)
 }
 
 /**
  * Parses a replay file buffer and returns its parsed info.
  */
-export async function getBufferRepInfo(arrayBuffer) {
+export async function getBufferRepInfo(arrayBuffer, options = {}) {
   const bufferSize = arrayBuffer.byteLength
   const replay = await getRepParsedData(arrayBuffer)
-  return generateRepInfo(replay, bufferSize)
+  return generateRepInfo(replay, bufferSize, options)
 }
